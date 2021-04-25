1603d326a7c060199a85dda0c5b73dd4684b321b
law <parash.sherpa@gmail.com>
2021-04-25 18:06:52 -0400

    Completed all the functionality of the debit component

--- src/components/Debit.js
+++ src/components/Debit.js
@@ -12,28 +12,13 @@ class Debit extends Component{
             sentinel: false,
             debit:{
                 description: "",
-                amount: 0
-            }
+                amount: 0,
+                date: ""
+            },
+            newEntry : []
         };
     }
 
-    FetchDebit = async ()=>{
-        try{
-            let response = await axios.get("https://moj-api.herokuapp.com/debits");
-            this.setState({apiData:response.data});
-            this.state.apiData.forEach(values => {
-                this.setState(prev => ({
-                    totalDebit: values.amount + prev.totalDebit
-                }));
-            });
-            
-        }
-        catch (error){
-            if(error.response){
-                console.log(error.response.data);
-            }
-        }
-    }
 
     createTable = ()=>{
        let currData = this.state.apiData;
@@ -52,8 +37,40 @@ class Debit extends Component{
         return table;
     }
 
-    componentDidMount(){
-        this.FetchDebit();
+    createEntry = ()=>{
+        let currData = this.state.newEntry;
+        console.log(currData);
+        let table = currData.map((tabData, index) =>{
+            
+            return(
+                 <tr key = {index}>
+                     <td>{tabData.description}</td>
+                     <td>{tabData.amount}</td>
+                     <td>{tabData.date}</td>
+                 </tr>)
+        });
+ 
+         
+         return table;
+     }
+
+    async componentDidMount(){
+        try{
+            let response = await axios.get("https://moj-api.herokuapp.com/debits");
+            let debitData = response.data
+            let debitTotal = 0;
+            debitData.forEach(deb => {
+                debitTotal += deb.amount;
+            })
+            this.setState({apiData: debitData, totalDebit:debitTotal });
+            this.props.dynamicBalance(this.state.totalDebit, this.state.sentinel);
+        }
+            
+        catch (error){
+            if(error.response){
+                console.log(error.response.data);
+            }
+        }
     }
 
     handleDescription = (e) => {
@@ -67,40 +84,64 @@ class Debit extends Component{
     handleAmount = (e) => {
         const newAmt = {...this.state.debit}
        const inputField = e.target.name
-       const inputValue = e.target.value
-       newAmt[inputField] = inputValue
-       this.setState({debit: newAmt});
+       let inputValue = e.target.value;
+       if(e.target.value){
+        inputValue = parseInt(e.target.value)
+       }
+       
+       newAmt[inputField] = inputValue;
+        this.setState({debit:newAmt});
     }
+    handleDate = (e)=>{
+        const newDate = {...this.state.debit}
+        const inputField = e.target.name 
+        const inputValue = e.target.value
+        newDate[inputField] = inputValue;
+        this.setState({debit:newDate});
+    }
+
 
     handleSubmit = (e)=>{
         e.preventDefault()
-        
+        let entry = [...this.state.newEntry];
+        entry.push(this.state.debit);
+        this.props.balanceAddition(this.state.debit.amount);
+        this.setState({newEntry: entry});
     }
 
 
     render(){
+        
         return(
             <div>
                 <h1>Debits</h1>
-                <Link to = "/"> Return Home </Link>
+                <Link className = "links" to = "/"> Return Home </Link>
+                
                 <form onSubmit={this.handleSubmit}> 
                 <div>
-                    <label htmlFor="descriptino">Description</label>
+                    <label htmlFor="description">Description</label>
                     <input type="text" name="description" onChange={this.handleDescription} value={this.state.debit.description} />
                 </div>
                 <div>
                     <label htmlFor="amount">Amount</label>
                     <input type="text" name="amount" onChange = {this.handleAmount} value = {this.state.debit.amount}/>
                 </div>
+                <div>
+                    <label htmlFor="date">Date</label>
+                    <input type="date" name="date" onChange = {this.handleDate} value = {this.state.debit.date}/>
+                </div>
                     <button>Add debit</button>
                 </form>
                 <table>
                     <tbody>
                         {this.createTable()}
+                        {this.createEntry()}
                     </tbody>
+                    
                 </table>
 
                 <h1>Account Balance</h1> 
+                <AccountBalance accountBalance = {this.props.accountBalance}/>
             </div>
         );
     }
