e7b8632168221ae5ad95476aae64b5721fda01ce
law <parash.sherpa@gmail.com>
2021-04-25 18:05:14 -0400

    Added link to login

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ import Credit from './components/Credit'
 class App extends Component {
   constructor(){
     super();
-    this.state = {accountBalance : 14568.27,
+    this.state = {accountBalance : 0,
+      creditValue: true, // Work around so that it only renders the total credit amount from the api call only once
       currentUser: {
                     userName: 'joe_shmo',
                     memberSince: '07/23/96'
-                  }
+                  },
+      flagValue : true, // Work around so that it only renders the total debit amount from the api call only once
+      
       };
   }
 
@@ -25,26 +28,52 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
-  dynamicBalance = (balanceInfo)=>{
+  dynamicBalance = (balanceInfo, sentinel)=>{
     let newBalance = this.state.accountBalance
-    newBalance = newBalance + balanceInfo
-    this.setState({accountBalance : newBalance})
+    newBalance = newBalance - balanceInfo;
+    if(this.state.flagValue){
+      this.setState({accountBalance : newBalance, flagValue:sentinel})
+    }
   }
+  
+  balanceAddition = (balance) =>{
+    let newBalance = this.state.accountBalance
+    newBalance = newBalance - balance;
+    this.setState({accountBalance: newBalance});
+  }
+
+
+  dynamicCredit = (info, sentinel) =>{
+    let newBalance = this.state.accountBalance
+    newBalance = newBalance + info;
+    if(this.state.creditValue){
+      this.setState({accountBalance : newBalance, creditValue:sentinel});
+    }
+  }
+
+  creditAddition = (balance) =>{
+    let newBalance = this.state.accountBalance
+    newBalance = newBalance + balance;
+    this.setState({accountBalance: newBalance});
+  }
+  
 
   render(){
     const HomeComponent = () => (<Home accountBalance = {this.state.accountBalance} />);
     const UserProfileComponent = () => (<UserProfile userName = {this.state.currentUser.userName} memberSince = {this.state.currentUser.memberSince} />);
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);
-    const DebitComponent = () => (<Debit dynamicBalance = {this.dynamicBalance}/>);
-    const CreditComponent = ()=>(<Credit/>);
+    const DebitComponent = () => (<Debit dynamicBalance = {this.dynamicBalance} accountBalance = {this.state.accountBalance} balanceAddition = {this.balanceAddition} />);
+    const CreditComponent = ()=>(<Credit dynamicCredit = {this.dynamicCredit} accountBalance = {this.state.accountBalance} creditAddition = {this.creditAddition} />);
     return (
       <Router>
           <div>
+            <Switch>
             <Route exact path="/" render={HomeComponent}/>
             <Route exact path="/userProfile" render={UserProfileComponent}/>
             <Route exact path="/login" render={LogInComponent}/>
             <Route exact path="/debit" render={DebitComponent} />
             <Route exact path="/credit" render={CreditComponent} />
+            </Switch>
           </div>
         </Router>
    );
