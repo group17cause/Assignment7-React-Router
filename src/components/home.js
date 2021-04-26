29458c9c79f9c8cbb8de0d1c4cd641013a9df03e
law <parash.sherpa@gmail.com>
2021-04-25 21:51:18 -0400

    added stylings

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import bank from "./../bank.jpg"
 import AccountBalance from './AccountBalance.js'
 import {Link} from 'react-router-dom'
     
@@ -6,7 +7,7 @@ import {Link} from 'react-router-dom'
       render() {
         return (
             <div>
-              <img src="../public/bank.jpg" alt="bank"/>
+              <img src={bank} alt="bank"/>
                 <h1>Bank of React</h1>
                <Link className = "links" to="/userProfile">User Profile</Link>
                <Link className = "links" to="/login">Login</Link>
